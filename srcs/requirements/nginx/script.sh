b45948a878188f5d97d2bfb0bfdcee0ace8ff37d
Niklas Burchhardt <niklas@burchhardt.de>
2026-05-10 19:34:49 +0200

    feat(nginx): configure SSL and expose HTTPS port 443

--- srcs/requirements/nginx/script.sh
+++ srcs/requirements/nginx/script.sh
@@ -3,12 +3,14 @@
 envsubst '${DOMAIN}' < "/etc/nginx/sites-available/default" > "/etc/nginx/sites-available/default.tmp"
 mv /etc/nginx/sites-available/default.tmp /etc/nginx/sites-available/default
 
-openssl req -x509 \
-			-nodes \
-			-days 365 \
-			-newkey rsa:2048 \
-			-keyout /etc/ssl/private/nginx-selfsigned.key \
-			-out /etc/ssl/certs/nginx-selfsigned.crt \
-			-subj "/C=DE/ST=BW/L=Heilbronn/O=42HN/CN=$DOMAIN"
+if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ]; then
+	openssl req -x509 \
+				-nodes \
+				-days 365 \
+				-newkey rsa:2048 \
+				-keyout /etc/ssl/private/nginx-selfsigned.key \
+				-out /etc/ssl/certs/nginx-selfsigned.crt \
+				-subj "/C=DE/ST=BW/L=Heilbronn/O=42HN/CN=$DOMAIN"
+fi
 
-nginx -g "daemon off;"
+exec nginx -g "daemon off;"
