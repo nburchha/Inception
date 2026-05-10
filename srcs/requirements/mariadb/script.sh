cd7807f52fc3cebbd39b08c2e92e5f777ea2683d
Niklas Burchhardt <niklas@burchhardt.de>
2026-05-10 19:34:55 +0200

    fix(mariadb): prevent db re-initialization and run mysqld with exec

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -3,5 +3,8 @@
 envsubst < /etc/mysql/init.sql > /etc/mysql/tmp.sql
 mv /etc/mysql/tmp.sql /etc/mysql/init.sql
 
-mysql_install_db
-mysqld
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mysql_install_db
+fi
+
+exec mysqld
